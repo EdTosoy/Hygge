48b7c4881302add4b34b69aea757f5e1bc961c62
edtosoy <edberto.tosoy@gmail.com>
2024-12-17 06:53:14 +0800

    back to pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, configDefaults } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,6 +24,7 @@ export default defineConfig({
         secure: false,
       },
     },
+    historyApiFallback: true, // Add this line
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), createHtmlPlugin()],
 });
