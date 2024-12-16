4020cc03a1c4984dcc0effd2817ef3e8335cb511
edtosoy <edberto.tosoy@gmail.com>
2024-12-17 06:07:28 +0800

    fix back end endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://hygge-backend-production.up.railway.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api/"),
         secure: false,
       },
     },
