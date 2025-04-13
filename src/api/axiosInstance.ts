cc35541122e35ea9be9ccf03e27c6d5984d64f0c
edtosoy <edberto.tosoy@gmail.com>
2025-04-13 15:07:56 +0800

    added auth header

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,28 @@
 import axios from "axios";
+import { USER_INFO } from "src/constants";
 
 // Create an Axios instance with default options
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   withCredentials: true,
  baseURL: "https://hygge-backend-production.up.railway.app"
 });
+
+
+// Add a request interceptor to include the token in headers
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const userInfo = localStorage.getItem(USER_INFO);
+    if (userInfo) {
+      const { token } = JSON.parse(userInfo); // Extract the token
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export { axiosInstance };
